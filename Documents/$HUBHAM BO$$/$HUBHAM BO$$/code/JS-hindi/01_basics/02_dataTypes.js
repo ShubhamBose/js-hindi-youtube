9ff915dc68d0945a3045ee377cb2849d247ea182
Shubham Bose <sboss0545@gmail.com>
2025-01-30 16:20:11 +0530

    add new practice for Javascript

---
+++ Documents/$HUBHAM BO$$/$HUBHAM BO$$/code/JS-hindi/01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3+ 3); //we are using nodejs, not browser
+
+let name = "shubham";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standerlone value
+// undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
---
+++ Documents/$HUBHAM BO$$/$HUBHAM BO$$/code/JS-hindi/01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "shubham";
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33sbc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"shubham" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
